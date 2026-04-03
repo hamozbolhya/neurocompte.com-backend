ca1dc865a3a4d874642a92e26fd20f7416600aeb
hamza boulahia <hamza.boulahia@procheck.ma>
2026-04-03 23:18:53 +0100

    commit changes

--- src/main/java/com/pacioli/core/repositories/UserRepository.java
+++ src/main/java/com/pacioli/core/repositories/UserRepository.java
@@ -1,10 +1,10 @@
 package com.pacioli.core.repositories;
 
 import com.pacioli.core.models.User;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,12 +13,12 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    @Query("SELECT u FROM User u WHERE u.id = :id")
-    Optional<User> findById(@Param("id") UUID id);
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
-    @Override
-    List<User> findAll(Sort sort);
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    @NonNull Optional<User> findById(@NonNull @Param("id") UUID id);
 
     List<User> findByCabinetId(Long cabinetId);
 

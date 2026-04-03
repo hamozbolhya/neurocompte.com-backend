f881910eefd99b5b18f7405f20b37ddbd6add8f7
hamza boulahia <hamza.boulahia@procheck.ma>
2026-04-03 23:22:41 +0100

    commit changes

--- src/main/java/com/pacioli/core/config/WebSocketConfig.java
+++ src/main/java/com/pacioli/core/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.pacioli.core.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -16,18 +17,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private AuthChannelInterceptor authChannelInterceptor;
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*");
     }
 
     @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
+    public void configureClientInboundChannel(@NonNull ChannelRegistration registration) {
         registration.interceptors(authChannelInterceptor);
     }
 }
